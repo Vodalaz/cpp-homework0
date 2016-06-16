e647bd92439ee8e6d263d67e48dfc264781b8ae4
Vodalaz <berinchik2012@gmail.com>
2016-06-16 15:15:47 +0300

    Update big_integer.h

--- bigint/big_integer.h
+++ bigint/big_integer.h
@@ -1,8 +1,11 @@
 #ifndef BIG_INTEGER_H
 #define BIG_INTEGER_H
-#include <iosfwd>
 
-const uint64_t BASE = (1ll << 32);
+
+typedef uint64_t ui64;
+
+const ui64 BASE = (1ll << 32);
+const int BASEPOW = 32;
 
 struct big_integer {
 public:
@@ -45,18 +48,17 @@ public:
     friend bool operator<=(big_integer const&, big_integer const&);
     friend bool operator>=(big_integer const&, big_integer const&);
 
-	friend std::string to_string(big_integer const&);
-
+	ui64 element(int pos) const;
 	int sz() const;
+	bool get_sgn() const;
+
 private:
-	void check_zero();
-	void binary_oper(big_integer const&, uint64_t(*)(uint64_t, uint64_t));
-	void reserve(int);
-	void clean();
-	int n_bits() const;
-	uint64_t *num;
-	uint32_t bits;
 	bool sgn;
+	std::vector< ui64 > vec;
+	void apply_binary(big_integer const&, ui64(*)(ui64, ui64));
+	void verify_sign();
+    void to_byte();
+    void from_byte();
 };
 
 int compare(big_integer const&, big_integer const&);
@@ -69,20 +71,26 @@ bool operator>=(big_integer const&, big_integer const&);
 
 std::string to_string(big_integer const&);
 
-big_integer operator+(big_integer const& a, big_integer const& b);
-big_integer operator-(big_integer const& a, big_integer const& b);
-big_integer operator*(big_integer const& a, big_integer const& b);
-big_integer operator/(big_integer const& a, big_integer const& b);
-big_integer operator%(big_integer const& a, big_integer const& b);
-big_integer operator&(big_integer const&, big_integer const&);
-big_integer operator|(big_integer const&, big_integer const&);
-big_integer operator^(big_integer const&, big_integer const&);
-big_integer operator>>(big_integer const&, int);
-big_integer operator<<(big_integer const&, int);
-uint64_t binary_and(uint64_t a, uint64_t b);
-uint64_t binary_or(uint64_t a, uint64_t b);
-uint64_t binary_xor(uint64_t a, uint64_t b);
+big_integer operator+(big_integer a, big_integer const& b);
+big_integer operator-(big_integer a, big_integer const& b);
+big_integer operator*(big_integer a, big_integer const& b);
+big_integer operator/(big_integer a, big_integer const& b);
+big_integer operator%(big_integer a, big_integer const& b);
+big_integer operator&(big_integer, big_integer const&);
+big_integer operator|(big_integer, big_integer const&);
+big_integer operator^(big_integer, big_integer const&);
+big_integer operator>>(big_integer, int);
+big_integer operator<<(big_integer, int);
 
 std::ostream& operator<<(std::ostream& s, big_integer const& a);
+
+void push(std::vector<ui64>&a, int pos);
+void del_zeros(std::vector<ui64>&);
+void mul_long_short(std::vector<ui64>& a, ui64 base, ui64 b);
+void div_long_short(std::vector<ui64>& a, ui64 base, ui64 b);
+void add_long_short(std::vector<ui64>&a, ui64 base, ui64 b);
+ui64 _and(ui64 a, ui64 b);
+ui64 _or(ui64 a, ui64 b);
+ui64 _xor(ui64 a, ui64 b);
 
 #endif
